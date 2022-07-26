a7bb8f1fb56ac4761fe01671cd3b21ad2cf7e2a5
BARR9898 <isc.brayan.rodriguez@gmail.com>
2022-07-25 21:53:09 -0500

    Inicio del repositorio

---
+++ index.js
@@ -0,0 +1,14 @@
+    
+    
+    
+    
+    
+    let createOrder = document.getElementById('createOrder')
+    createOrder.addEventListener('click', () => {
+        alert('Creating order')
+    })
+
+
+
+
+
